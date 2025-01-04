2591a1e1018c5c56d53937aede2763f863ca34c5
Chetan Budathoki <chetanbudathoki@outlook.com>
2025-01-04 18:42:59 +0000

    "long data table"

--- database/members.go
+++ database/members.go
@@ -3,24 +3,36 @@ package database
 
 import (
 	"log"
+	"time"
 )
 
 // Member represents a row in the members table
 type Member struct {
-	FirstName    string `json:"first_name"`
-	LastName     string `json:"last_name"`
-	MiddleName   string `json:"middle_name"`
-	EmailID      string `json:"email_id"`
-	MobileNumber string `json:"mobile_number"`
-	PermanentDistrict string `json:"permanent_district"`
-	PermanentPalika   string `json:"permanent_palika"`
-	PermanentWada     int    `json:"permanent_wada"`
-	PermanentTole     string `json:"permanent_tole"`
+	ID                 int       `json:"id"`
+	FirstName          string    `json:"first_name"`
+	LastName           string    `json:"last_name"`
+	MiddleName         string    `json:"middle_name"`
+	EmailID            string    `json:"email_id"`
+	MobileNumber       string    `json:"mobile_number"`
+	Gender             string    `json:"gender"`
+	DOB                string    `json:"dob"`
+	BloodGroup         string    `json:"blood_group"`
+	PermanentDistrict  string    `json:"permanent_district"`
+	PermanentPalika    string    `json:"permanent_palika"`
+	PermanentWada      int       `json:"permanent_wada"`
+	PermanentTole      string    `json:"permanent_tole"`
+	TemporaryDistrict  string    `json:"temporary_district"`
+	TemporaryPalika    string    `json:"temporary_palika"`
+	TemporaryWada      int       `json:"temporary_wada"`
+	TemporaryTole      string    `json:"temporary_tole"`
+	CreatedAt          time.Time `json:"created_at"`
 }
 
 func FetchMembers() ([]Member, error) {
-	query := `SELECT first_name, last_name, middle_name, email_id, mobile_number, 
-	                 permanent_district, permanent_palika, permanent_wada, permanent_tole 
+	// Adjust the query to fetch all columns
+	query := `SELECT id, first_name, last_name, middle_name, email_id, mobile_number, gender, dob, blood_group,
+	                 permanent_district, permanent_palika, permanent_wada, permanent_tole,
+	                 temporary_district, temporary_palika, temporary_wada, temporary_tole, created_at
 	          FROM members`
 
 	// Execute the query
@@ -35,9 +47,11 @@ func FetchMembers() ([]Member, error) {
 	var members []Member
 	for rows.Next() {
 		var member Member
-		err := rows.Scan(&member.FirstName, &member.LastName, &member.MiddleName, 
-			&member.EmailID, &member.MobileNumber, 
-			&member.PermanentDistrict, &member.PermanentPalika, &member.PermanentWada, &member.PermanentTole)
+		err := rows.Scan(&member.ID, &member.FirstName, &member.LastName, &member.MiddleName,
+			&member.EmailID, &member.MobileNumber, &member.Gender, &member.DOB, &member.BloodGroup,
+			&member.PermanentDistrict, &member.PermanentPalika, &member.PermanentWada, &member.PermanentTole,
+			&member.TemporaryDistrict, &member.TemporaryPalika, &member.TemporaryWada, &member.TemporaryTole,
+			&member.CreatedAt)
 		if err != nil {
 			log.Printf("Error scanning row: %v", err)
 			continue
@@ -52,4 +66,3 @@ func FetchMembers() ([]Member, error) {
 
 	return members, nil
 }
-
